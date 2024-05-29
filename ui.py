295c0365dcf4f37bdb91164422dd0be61e15b1be
advargin <advargin@gmail.com>
2024-05-29 15:26:49 +0300

    -

---
+++ ui.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'calc.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(480, 688)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setStyleSheet("background-color: #22222e")
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 481, 270))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.frame.setFont(font)
+        self.frame.setStyleSheet("background-color:green")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(120, 10, 221, 91))
+        font = QtGui.QFont()
+        font.setFamily("18")
+        font.setPointSize(24)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.label.setFont(font)
+        self.label.setAutoFillBackground(False)
+        self.label.setStyleSheet("font:Impact 18;\n"
+"color: white")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(50, 70, 271, 221))
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("../Downloads/pngwing.com.png"))
+        self.label_2.setScaledContents(True)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.frame)
+        self.label_3.setGeometry(QtCore.QRect(150, 90, 141, 161))
+        self.label_3.setText("")
+        self.label_3.setPixmap(QtGui.QPixmap("pngwing.com.png"))
+        self.label_3.setScaledContents(True)
+        self.label_3.setObjectName("label_3")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(50, 320, 380, 60))
+        font = QtGui.QFont()
+        font.setFamily("Impact")
+        font.setPointSize(14)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet("background-color: #22222e;\n"
+"border: 2px solid #f66867;\n"
+"border-radius: 30;\n"
+"color: white;")
+        self.lineEdit.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(50, 530, 380, 60))
+        font = QtGui.QFont()
+        font.setFamily("Impact")
+        font.setPointSize(14)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setStyleSheet("background-color: #22222e;\n"
+"border: 2px solid #f66867;\n"
+"border-radius: 30;\n"
+"color: white;")
+        self.lineEdit_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(50, 460, 380, 60))
+        font = QtGui.QFont()
+        font.setFamily("Impact")
+        font.setPointSize(14)
+        self.lineEdit_3.setFont(font)
+        self.lineEdit_3.setStyleSheet("background-color: #22222e;\n"
+"border: 2px solid #f66867;\n"
+"border-radius: 30;\n"
+"color: white;")
+        self.lineEdit_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_4.setGeometry(QtCore.QRect(50, 390, 380, 60))
+        font = QtGui.QFont()
+        font.setFamily("Impact")
+        font.setPointSize(14)
+        self.lineEdit_4.setFont(font)
+        self.lineEdit_4.setStyleSheet("background-color: #22222e;\n"
+"border: 2px solid #f66867;\n"
+"border-radius: 30;\n"
+"color: white;")
+        self.lineEdit_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(50, 600, 380, 60))
+        font = QtGui.QFont()
+        font.setFamily("Impact")
+        font.setPointSize(14)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("QPushButton { background-color: green; border-radius: 30;\n"
+"color: white;\n"
+"}QPushButton: pressed { background-color: blue; }")
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.frame.setProperty("background-color", _translate("MainWindow", "#fb5b5d"))
+        self.label.setText(_translate("MainWindow", "Конвертер валют"))
+        self.pushButton.setText(_translate("MainWindow", "Конвертация"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+
